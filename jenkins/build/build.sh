bd5694532772971e2259b11f2ce390b1b082d5d9
root <root@UbuntuUptest.cygdc.local>
2021-09-07 05:15:45 -0400

    testing

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -4,7 +4,8 @@ echo "******************************"
 echo "*****BUILD Creation*************"
 echo "******************************"
 
-WORKSPACE=/home/cyg_adm/jenkins_home/workspace/pipeline-nodejs-project
-docker run --rm -v $WORKSPACE/nodejs-app:/app -v /root/.m2:/root/.m2 -w /app node:12.18.1 npm install 
-docker run --rm -v $WORKSPACE/nodejs-app:/app -v /root/.m2:/root/.m2 -w /app node:12.18.1 npm test
+docker run -ti -v /home/cyg_adm/pipeline-nodejs-project/nodejs-app:/app -v /root/.m2:/root/.m2 --name nodejs-app -w /app node:12.18.1 npm install 
+docker rm -f nodejs-app
+docker run -ti -v /home/cyg_adm/pipeline-nodejs-project/nodejs-app:/app -v /root/.m2:/root/.m2 --name nodejs-app -w /app node:12.18.1 npm test
+docker rm -f nodejs-app
 
