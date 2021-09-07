982b8e6dd2ec7683e62ca09cc70778a267d8957f
root <root@UbuntuUptest.cygdc.local>
2021-09-07 03:58:50 -0400

    change in build file

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -5,5 +5,6 @@ echo "*****BUILD Creation*************"
 echo "******************************"
 
 WORKSPACE=/home/cyg_adm/jenkins_home/workspace/pipeline-nodejs-project
-docker run --rm -v $WORKSPACE/nodejs-app:/app -v /root/.m2:/root/.m2 -w /app node:12.18.1 "$@"
+docker run -rm -ti -v $WORKSPACE/nodejs-app:/app -v /root/.m2:/root/.m2 --name nodejs-test -w /app node:12.18.1 npm install
+docker run -rm -ti -v $WORKSPACE/nodejs-app:/app -v /root/.m2:/root/.m2 --name nodejs-test -w /app node:12.18.1 npm test
 
