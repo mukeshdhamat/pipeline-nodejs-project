4d0a00529532ca1a43e3e9082096093c27d1765f
root <root@UbuntuUptest.cygdc.local>
2021-09-07 05:44:38 -0400

    updated buildimage

--- jenkins/buildimage/buildimage.sh
+++ jenkins/buildimage/buildimage.sh
@@ -1,5 +1,5 @@
 echo "**************************************"
 echo "******Building Docker Image******************"
 echo "**************************************"
-cd /home/cyg_adm/pipeline-nodejs-project/nodejs-app && docker-compose build --no-cache
+cd jenkins/buildimage && docker-compose build --no-cache
 
